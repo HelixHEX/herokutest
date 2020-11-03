7a65e2943935f1c382b7ed84a6ba515d6a2aefa6
HelixHEX <heyy.im.eli@gmail.com>
2020-11-02 21:34:01 -0500

    Revert ":fire: Login page done"
    This reverts commit cc1c310a4d915e7bdcac3d97cd624ae6847e6aae.

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,79 +1,13 @@
-import React from "react";
-import { useHistory } from "react-router-dom";
+import React from 'react'
 
-// @ts-ignore
-import { store, useGlobalState } from "state-pool";
+interface loginProps {
 
-//chakra ui
-import { useToast, Input, Button, Flex, useColorMode } from "@chakra-ui/core";
-import Navbar from "./Navbar";
-
-interface loginProps {}
-
-store.setState("user", { username: "" });
+}
 
 const Login: React.FC<loginProps> = () => {
-  const toast = useToast();
+    return (
+      <h1>Login Page</h1>
+    );
+}
 
-  // eslint-disable-next-line
-  const { colorMode, toggleColorMode } = useColorMode();
-  let history = useHistory();
-
-  // eslint-disable-next-line
-  const [user, updateUser, _] = useGlobalState("user");
-  const handleChange = (event: any) => {
-    updateUser((user: any) => {
-      user.username = event.target.value;
-    });
-  };
-
-  const submit = async (e: any) => {
-    e.preventDefault();
-    if (user.username.length < 3) {
-      toast({
-        title: "An error occurred.",
-        description: "Please enter passowrd longer than 3 characters",
-        status: "error",
-        duration: 9000,
-        isClosable: true,
-      });
-    } else {
-      updateUser((username: any) => {
-        username.username = user.username;
-        history.push("/chat");
-      });
-    }
-  };
-  return (
-    <>
-      <Navbar />
-      <form onSubmit={submit}>
-        <Flex
-          bg={colorMode === "light" ? "gray.800" : "gray.700"}
-          margin="auto"
-          flexWrap="wrap"
-          mt="15%"
-          align="center"
-          height="200px"
-          width="500px"
-          rounded="5px"
-          justifyContent="space-between"
-          padding="5%"
-        >
-          <Input
-            bg={colorMode === "light" ? "blue" : "pink.400"}
-            placeholder="Username"
-            value={user.username}
-            width="75%"
-            onChange={handleChange}
-          />
-          <Button type="submit" variantColor="pink">
-            Login
-          </Button>
-        </Flex>
-      </form>
-    </>
-  );
-};
-
-export default Login;
+export default Login
--- src/index.tsx
+++ src/index.tsx
@@ -1,53 +1,41 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-import {
-  Provider,
-  subscriptionExchange,
-  createClient,
-  defaultExchanges,
-} from "urql";
-import { SubscriptionClient } from "subscriptions-transport-ws";
+// import {
+//   // Provider,
+//   subscriptionExchange,
+//   createClient,
+//   defaultExchanges,
+// } from "urql";
+// import { SubscriptionClient } from "subscriptions-transport-ws";
 
-// chakra ui
-import {
-  CSSReset,
-  ThemeProvider,
-  theme,
-  ColorModeProvider,
-} from "@chakra-ui/core";
+//chakra ui
+// import { CSSReset, ThemeProvider, theme } from '@chakra-ui/core'
 
-const subscriptionClient = new SubscriptionClient(
-  "ws://25fe7a9f964b.ngrok.io/graphql",
-  {
-    reconnect: true,
-  }
-);
+// const subscriptionClient = new SubscriptionClient(
+//   "ws://25fe7a9f964b.ngrok.io/graphql",
+//   {
+//     reconnect: true, 
+//   }
+// );
 
-const client = createClient({
-  url: "https://25fe7a9f964b.ngrok.io/graphql",
-  exchanges: [
-    ...defaultExchanges,
-    subscriptionExchange({
-      forwardSubscription: (operation) => subscriptionClient.request(operation),
-    }),
-  ],
-});
+// const client = createClient({
+//   url: "https://25fe7a9f964b.ngrok.io/graphql",
+//   exchanges: [
+//     ...defaultExchanges,
+//     subscriptionExchange({
+//       forwardSubscription: (operation) => subscriptionClient.request(operation),
+//     }),
+//   ],
+// });
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <ColorModeProvider>
-        <Provider value={client}>
-          <App />
-        </Provider>
-        <CSSReset />
-      </ColorModeProvider>
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function

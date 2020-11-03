cc1c310a4d915e7bdcac3d97cd624ae6847e6aae
HelixHEX <heyy.im.eli@gmail.com>
2020-11-02 21:29:02 -0500

    :fire: Login page done

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+//chakra ui
+import { Flex, useColorMode, IconButton } from "@chakra-ui/core";
+
+interface NavbarProps {}
+
+const Navbar: React.FC<NavbarProps> = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <>
+      <Flex>
+        <IconButton
+          variant="outline"
+          aria-label="Toggle dark mode"
+          icon={colorMode === "light" ? "moon" : "sun"}
+          onClick={toggleColorMode}
+        />
+      </Flex>
+    </>
+  );
+};
+
+export default Navbar;

5483256d7624da22c29684fc9b0ffea1f93df666
HelixHEX <heyy.im.eli@gmail.com>
2020-11-03 00:03:15 -0500

    removed chakra ui

--- src/index.tsx
+++ src/index.tsx
@@ -3,49 +3,49 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-import {
-  Provider,
-  subscriptionExchange,
-  createClient,
-  defaultExchanges,
-} from "urql";
-import { SubscriptionClient } from "subscriptions-transport-ws";
+// import {
+//   Provider,
+//   subscriptionExchange,
+//   createClient,
+//   defaultExchanges,
+// } from "urql";
+// import { SubscriptionClient } from "subscriptions-transport-ws";
 
-// chakra ui
-import {
-  CSSReset,
-  ThemeProvider,
-  theme,
-  ColorModeProvider,
-} from "@chakra-ui/core";
+// // chakra ui
+// import {
+//   CSSReset,
+//   ThemeProvider,
+//   theme,
+//   ColorModeProvider,
+// } from "@chakra-ui/core";
 
-const subscriptionClient = new SubscriptionClient(
-  "ws://25fe7a9f964b.ngrok.io/graphql",
-  {
-    reconnect: true,
-  }
-);
+// const subscriptionClient = new SubscriptionClient(
+//   "ws://25fe7a9f964b.ngrok.io/graphql",
+//   {
+//     reconnect: true,
+//   }
+// );
 
-const client = createClient({
-  url: "https://25fe7a9f964b.ngrok.io/graphql",
-  exchanges: [
-    ...defaultExchanges,
-    subscriptionExchange({
-      forwardSubscription: (operation) => subscriptionClient.request(operation),
-    }),
-  ],
-});
+// const client = createClient({
+//   url: "https://25fe7a9f964b.ngrok.io/graphql",
+//   exchanges: [
+//     ...defaultExchanges,
+//     subscriptionExchange({
+//       forwardSubscription: (operation) => subscriptionClient.request(operation),
+//     }),
+//   ],
+// });
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    {/* <ThemeProvider theme={theme}>
       <ColorModeProvider>
-        <Provider value={client}>
+        <Provider value={client}> */}
           <App />
-        </Provider>
+        {/* </Provider>
         <CSSReset />
       </ColorModeProvider>
-    </ThemeProvider>
+    </ThemeProvider> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
